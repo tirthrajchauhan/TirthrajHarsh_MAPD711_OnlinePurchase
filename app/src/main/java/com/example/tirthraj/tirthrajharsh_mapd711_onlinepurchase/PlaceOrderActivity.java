945722968bbc3db879288f82a6762588039cf9e6
tirth456 <tirthraj456@gmail.com>
2018-11-01 15:59:34 -0400

    Added layout

---
+++ app/src/main/java/com/example/tirthraj/tirthrajharsh_mapd711_onlinepurchase/PlaceOrderActivity.java
@@ -0,0 +1,13 @@
+package com.example.tirthraj.tirthrajharsh_mapd711_onlinepurchase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaceOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_order);
+    }
+}
