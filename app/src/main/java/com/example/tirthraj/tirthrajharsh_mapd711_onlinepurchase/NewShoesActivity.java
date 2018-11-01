6c2fc14ef3ddd781995c393175169b91d37a84a0
tirth456 <tirthraj456@gmail.com>
2018-11-01 17:00:19 -0400

    Added Missing layout

---
+++ app/src/main/java/com/example/tirthraj/tirthrajharsh_mapd711_onlinepurchase/NewShoesActivity.java
@@ -0,0 +1,13 @@
+package com.example.tirthraj.tirthrajharsh_mapd711_onlinepurchase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewShoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_shoes);
+    }
+}
